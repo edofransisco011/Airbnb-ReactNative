7a3273825a96a2f38de66ce7a7a0ab7aa8f912f2
edofransisco011 <edofransisco011@gmail.com>
2023-11-17 01:06:13 +0700

    Working on _layout.tsx rn

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,32 @@
-import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router';
+import Colors from '@/constants/Colors';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 
 const Layout = () => {
-  return (
-<Tabs>
+    return (
+        <Tabs screenOptions={{
+            tabBarActiveTintColor: Colors.primary,
+            tabBarLabelStyle: {
 
-</Tabs>
-  )
+                fontFamily: 'mon-sb',
+            },
+        }}>
+            <Tabs.Screen name="index" options={{
+                tabBarLabel: 'Explore',
+                tabBarIcon: ({color, size}) => <Ionicons name="search" color={color} size={size} />,
+            }}
+            />
+
+            <Tabs.Screen name="wishlists" options={{
+                tabBarLabel: 'Wishlists',
+                tabBarIcon: ({color, size}) => <Ionicons name="heart-outline" color={color} size={size} />,
+            }}
+            />
+        </Tabs>
+    )
 }
 
 export default Layout
