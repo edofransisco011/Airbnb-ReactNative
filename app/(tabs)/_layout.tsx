a89df3618f076f39caa69079eb591713d879a87f
edofransisco011 <edofransisco011@gmail.com>
2023-11-20 22:46:40 +0700

    All Done

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,32 +1,63 @@
-import React from 'react'
 import { Tabs } from 'expo-router';
-import Colors from '@/constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
 import { FontAwesome5 } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { Ionicons } from '@expo/vector-icons';
+import Colors from '@/constants/Colors';
 
 const Layout = () => {
-    return (
-        <Tabs screenOptions={{
-            tabBarActiveTintColor: Colors.primary,
-            tabBarLabelStyle: {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors.primary,
+        tabBarLabelStyle: {
+          fontFamily: 'mon-sb',
+        },
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarLabel: 'Explore',
+          tabBarIcon: ({ size, color }) => <Ionicons name="search" size={size} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="whishlists"
+        options={{
+          tabBarLabel: 'Wishlists',
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="heart-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="trips"
+        options={{
+          tabBarLabel: 'Trips',
+          tabBarIcon: ({ size, color }) => <FontAwesome5 name="airbnb" size={size} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="inbox"
+        options={{
+          tabBarLabel: 'Inbox',
+          tabBarIcon: ({ size, color }) => (
+            <MaterialCommunityIcons name="message-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarLabel: 'Profile',
 
-                fontFamily: 'mon-sb',
-            },
-        }}>
-            <Tabs.Screen name="index" options={{
-                tabBarLabel: 'Explore',
-                tabBarIcon: ({color, size}) => <Ionicons name="search" color={color} size={size} />,
-            }}
-            />
+          headerShown: false,
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="person-circle-outline" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
 
-            <Tabs.Screen name="wishlists" options={{
-                tabBarLabel: 'Wishlists',
-                tabBarIcon: ({color, size}) => <Ionicons name="heart-outline" color={color} size={size} />,
-            }}
-            />
-        </Tabs>
-    )
-}
-
-export default Layout
+export default Layout;
